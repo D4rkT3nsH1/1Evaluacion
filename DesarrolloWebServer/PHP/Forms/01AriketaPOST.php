79e73629f041b28eee47aecd7eadf023e53447b0
Gontzal <gontzalizurza@gmail.com>
2023-09-21 13:39:35 +0200

    Agenda PHP

--- DesarrolloWebServer/PHP/Forms/01AriketaPOST.php
+++ DesarrolloWebServer/PHP/Forms/01AriketaPOST.php
@@ -1,37 +1,65 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+    <title>Datuak</title>
 </head>
-
 <body>
-  <h1>Datos personales del formulario recibido</h1>
+    <h1>Datu pertsonalak</h1>
+    <?php
+    if (isset($_POST['submit'])) {
+        $nombre = $_POST['name'];
+        $apellidos = $_POST['apellidos'];
+        $edad = $_POST['edad'];
+        $peso = $_POST['peso'];
+        $genero = $_POST['sexo'];
+        $estado_civil = $_POST['estado_civil'];
+        $afi = isset($_POST['afi']) ? $_POST['afi'] : array();
 
-  <style></style>
+        if (empty($nombre)) {
+            echo '<p style="color: red;">Izena ez da sartu</p>';
+        } else {
+            echo '<p>Zure izena <strong>' . $nombre . '</strong> da.</p>';
+        }
 
-  <fieldset>
-    <legend>Formulario recibido</legend>
-    
-    Nombre:
-    <?php isset($_POST["name"]) ? print $_POST["name"] : ""; ?><br>
-    Apellidos:
-    <?php isset($_POST["apellidos"]) ? print $_POST["apellidos"] : ""; ?><br>
-    Edad:
-    <?php isset($_POST["edad"]) ? print $_POST["edad"] : ""; ?><br>
-    Peso:
-    <?php isset($_POST["peso"]) ? print $_POST["peso"] : ""; ?><br>
-    Sexo:
-    <?php isset($_POST["sexo"]) ? print $_POST["sexo"] : ""; ?><br>
-    Estado civil:
-    <?php isset($_POST["ec"]) ? print $_POST["ec"] : ""; ?><br>
-    Aficiones:
-    <?php isset($_POST["afi"]) ? print $_POST["afi"] : ""; ?><br>
+        if (empty($apellidos)) {
+            echo '<p style="color: red;">Abizenak ez dira sartu</p>';
+        } else {
+            echo '<p>Zure abizenak <strong>' . $apellidos . '</strong> dira.</p>';
+        }
 
-  </fieldset>
+        if (empty($edad)) {
+            echo '<p style="color: red;">Adina ez da sartu</p>';
+        } else {
+            echo '<p>Zure adina <strong>' . $edad . '</strong> da.</p>';
+        }
 
+        if (empty($peso)) {
+            echo '<p style="color: red;">Pisua ez da sartu</p>';
+        } else {
+            echo '<p>Zure pisua <strong>' . $peso . ' kg</strong> da.</p>';
+        }
+
+        if (!empty($genero)) {
+            echo '<p>Zure sexua <strong>' . $genero . '</strong> da.</p>';
+        }
+
+        if ($estado_civil == "soltero") {
+            echo '<p>Zure estatua soltero da.</p>';
+        } elseif ($estado_civil == "casado") {
+            echo '<p>Zure estatua casado da.</p>';
+        } else {
+            echo '<p>Zure estatua beste da.</p>';
+        }
+
+        if (!empty($afi)) {
+            echo '<p>Zure gustokoak dira: <strong>' . implode(", ", $afi) . '</strong>.</p>';
+        } else {
+            echo '<p>Gustokoak ez dira sartu</p>';
+        }
+    } else {
+        echo '<p style="color: red;">Datuak ez dira bidali</p>';
+    }
+    ?>
+    <a href="01Ariketa.php">Itzuli formularioa</a>
 </body>
-
 </html>

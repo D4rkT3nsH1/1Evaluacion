d6cc63298dc24df016852989731ddcc00b933a61
Gontzal <gontzalizurza@gmail.com>
2023-09-22 14:49:41 +0200

    Arrays nueva ariketa

---
+++ DesarrolloWebServer/PHP/Array/07Ariketa.php
@@ -0,0 +1,29 @@
+<?php
+
+$estatuak = array(
+    'Frantzia' => 67000000,
+    'Mali' => 250000,
+    'Brasil' => 300000000,
+    'Pakistan' => 350000000,
+    'Marshall Irlak' => 55000
+);
+
+
+$biztanleria = array(
+    '0-100000-jenderik gabea',
+    '100001-1000000-jende gutxiduna',
+    '1000001-100000000-populatua',
+    '100000001-1000000000-oso populatua'
+);
+
+$kontinenteak = array(
+    'Europa' => 'Frantzia, Portugal, Italia',
+    'Asia' => 'China, Turquia, Pakistan',
+    'Afrika' => 'Nigeria, Maroko, Mali',
+    'Amerika' => 'EEUU, Brasil, Argentina',
+    'Ozeania' => 'Australia, Polinesia, Marshall Irlak'
+);
+
+
+
+?>

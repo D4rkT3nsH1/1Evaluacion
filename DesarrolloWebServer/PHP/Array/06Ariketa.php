cdb6e5afa07d5ce61f165d67695f90a038711de6
Gontzal <gontzalizurza@gmail.com>
2023-11-07 08:39:37 +0100

    Azterketa

--- DesarrolloWebServer/PHP/Array/06Ariketa.php
+++ DesarrolloWebServer/PHP/Array/06Ariketa.php
@@ -2,6 +2,10 @@
 
 $array = array("Iker" => 1, "Arkaitz" => 2, "Gontzal" => 3, "Jon" => 4);
 
+foreach ($array as $nombre) {
+    echo "$nombre <br>";
+}
+
 foreach ($array as $nombre => $valor) {
     echo "$nombre => $valor <br>";
 }

0fe0e3587affd707a48d201bdf034a73a2b54592
D4rkT3nsH1 <gontzalizurza@gmail.com>
2024-01-09 14:50:24 +0100

    uwu

--- DesarrolloWebCliente/IKASLEAK/js/app.js
+++ DesarrolloWebCliente/IKASLEAK/js/app.js
@@ -3,33 +3,90 @@ var app = angular.module('animalApp', []);
 
 app.controller('AnimalController', function ($scope, $http) {
     $scope.animals = [];
-    $scope.results = [];
+    $scope.results = {};
     $scope.gameStarted = false;
 
     // Cargar el arreglo de animales desde el archivo JSON
     $http.get('json/animales.json')
         .then(function (response) {
             $scope.animals = response.data;
+            console.log($scope.animals);
         })
         .catch(function (error) {
             console.error('Error al cargar el archivo JSON:', error);
         });
 
-    $scope.startGame = function (level) {
-        // Lógica para iniciar el juego con el nivel seleccionado
-        $scope.gameStarted = true;
+    $scope.start = function () {
+        $('#zonatest').show();
     };
 
-    $scope.checkAnswer = function (selectedAnswer, correctAnswer) {
-        // Lógica para verificar la respuesta del usuario
-        if (selectedAnswer === correctAnswer) {
-            $scope.results.push('Correcto');
-        } else {
-            $scope.results.push('Incorrecto');
+    $scope.startGame = function (level) {
+        // Lógica para iniciar el juego
+        $scope.gameStarted = true;
+        switch (level) {
+            case "facil":
+                $scope.filteredAnimals = $scope.animals.filter(function (animal) {
+                    return animal.tipo === "0";
+                });
+                break;
+
+            case "medio":
+                $scope.filteredAnimals = $scope.animals.filter(function (animal) {
+                    return animal.tipo === "1";
+                });
+                break;
+
+            case "dificil":
+                $scope.filteredAnimals = $scope.animals.filter(function (animal) {
+                    return animal.tipo === "2";
+                });
+                break;
         }
     };
 
-    $scope.loadResults = function () {
-        // Lógica para cargar resultados
+    $scope.checkAnswer = function (event, id, selectedAnswer, correctIndex) {
+        var elementoDeDatos = $scope.animals.find(function (elemento) {
+            return elemento.id === id;
+        });
+
+        if (elementoDeDatos.answered == "false") {
+            var correctIndex = "R" + ++correctIndex;
+            var correctAnimalName = elementoDeDatos[correctIndex];
+
+            if (selectedAnswer === correctAnimalName) {
+                $scope.results[selectedAnswer] = 'Correcto';
+            } else {
+                $scope.results[selectedAnswer] = 'Incorrecto';
+            }
+
+            elementoDeDatos.answered = true;
+            event.target.classList.add('seleccionado');
+
+            console.log("Animal ID:", id);
+            console.log("Selected Answer:", selectedAnswer);
+            console.log("Correct Index:", correctIndex);
+            console.log($scope.results);
+
+            if (Object.keys($scope.results).length == $scope.filteredAnimals.length) {
+                $('#zonaresultados').show();
+            }
+
+        } else {
+            console.log("Este animal ya ha sido respondido.");
+        }
     };
+
+    $scope.checkResults = function () {
+
+    }
+
+    $scope.resetGame = function () {
+        $scope.animals.forEach(function (animal) {
+            animal.answered = "false";
+        });
+        $scope.results = {};
+        $scope.gameStarted = false;
+        $('.seleccionado').removeClass('seleccionado');
+        $('#zonaresultados').hide();
+    }
 });
